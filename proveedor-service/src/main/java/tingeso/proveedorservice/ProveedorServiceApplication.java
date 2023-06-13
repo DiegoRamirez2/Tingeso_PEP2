e9951997f050afb97dfecb2868b08bbbe04d8bdc
DiegoRamirez2 <diego.ramirez.vi@usach.cl>
2023-06-13 07:04:35 -0400

    Agregacion de microservicios

--- proveedor-service/src/main/java/tingeso/proveedorservice/ProveedorServiceApplication.java
+++ proveedor-service/src/main/java/tingeso/proveedorservice/ProveedorServiceApplication.java
@@ -2,8 +2,9 @@ package tingeso.proveedorservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
+@EnableEurekaClient
 public class ProveedorServiceApplication {
 
 	public static void main(String[] args) {
